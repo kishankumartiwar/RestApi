0899891ceabe4026f773a82f808368d7ddf4e92d
kishankumartiwar <tiwarikishan297@gmail.com>
2025-02-08 18:51:27 +0530

    commmit

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const fs = require("fs");
 const users = require("./MOCK_DATA.json");
 const app = express();
 
 PORT = 8000;
+
+app.use(express.urlencoded({extended : false}));
 app.get("/api/users", (req,res) =>{
   return res.send(users);
 });
@@ -10,14 +13,27 @@ app.route("/api/users/:userId").get ((req,res) =>{
   const id = Number(req.params.userId);
   const user = users.find((user) => user.id === id);
   return res.json(user);
-}).patch((req,res) => {
-  return res.json({status: pending});
-}).delete ((req,res) => {
-  return res.json({status: pending});
+})
+.patch((req,res) => {
+  const id = Number(req.params.userId);
+  const user = users.find((user) => user.id === id);
+  const {first_name,last_name} = req.body; 
+  user.first_name = first_name; 
+  user.last_name = last_name;
+  console.log(user);
+ /*  fs.writeFileSync({}); */
+
+  return res.json({status: "pending"});
+})
+ .delete ((req,res) => {
+  return res.json({status: "pending"});
 });
 app.post("/api/users", (req,res) => {
-  console.log("status pending");
-  return res.json({status:pending});
+  const body = req.body;
+  users.push({...body , id: users.length+1});
+  fs.writeFileSync('./MOCK_DATA.json',JSON.stringify(users));
+  console.log(body);
+  return res.json({status:"success" , id: users.length });
 });
 
 app.get("/users" , (req,res)=>{
